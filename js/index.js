ab6d73cbae1d0a82c5ba1cd19c59aef6de97b529
Olexiy-repin <repinlyoha@gmail.com>
2022-12-11 22:40:33 +0200

    classwork

---
+++ js/index.js
@@ -0,0 +1,3 @@
+const number = 20;
+
+console.log(number);
---
+++ js/script.js
@@ -0,0 +1,44 @@
+// http://latentflip.com/loupe/?code=Y29uc29sZS5sb2coJ2ZpcnN0Jyk7CgpzZXRUaW1lb3V0KGZ1bmN0aW9uKCkgewogICAgY29uc29sZS5sb2coJ2lubmVyIHNldFRpbWVvdXQnKTsKfSwgMTAwMCk7Cgpjb25zb2xlLmxvZygnc2Vjb25kJyk7!!!
+
+//? Приведення типів
+// console.log(2 + 5 + '' + 1 + 0);
+// console.log('' + 1 - 0);
+// console.log(true + false);
+// console.log(6 / '3');
+// console.log('2' + '3');
+// console.log(4 + 5 + 'px');
+// console.log('$' + 4 + 5);
+// console.log('4' - 2);
+// console.log('4px' - 2);
+// console.log(7 / 0);
+// console.log('  -9  ' + 5);
+// console.log(null + 1);
+// console.log(6 + true);
+
+// function greet() {
+//   const number = 20;
+
+//   if (true) {
+//     console.log(number);
+//   }
+// }
+
+// greet();
+
+// function f1() {
+//   console.log('f1');
+//   f2();
+// }
+
+// function f2() {
+//   console.log('f2');
+//   f3();
+// }
+
+// function f3() {
+//   console.log('f3');
+// }
+
+// f1();
+
+const number = 10;
