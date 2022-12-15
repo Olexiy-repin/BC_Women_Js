2b6daa8924973fb4b831ee17726a60f6df0089ef
Olexiy-repin <repinlyoha@gmail.com>
2022-12-15 11:00:25 +0200

    classwork

--- js/arrows-function.js
+++ js/arrows-function.js
@@ -41,23 +41,21 @@
 
 // console.log(sum(10, 20));
 
-const createProduct = product => {};
+// const createProduct = product => ({ id: Date.now(), ...product });
+
+// console.log(createProduct({ firstName: 'Jennie', lastName: 'Malone', age: 30 }));
 
 /*
 ? Виконайте рефакторинг коду за допомогою стрілочних функцій.
 */
-// function createProduct(partialProduct, callback) {
+// const createProduct = (partialProduct, callback) => {
 //   const product = { id: Date.now(), ...partialProduct };
+
 //   callback(product);
-// }
+// };
 
-// function logProduct(product) {
-//   console.log(product);
-// }
-
-// function logTotalPrice(product) {
-//   console.log(product.price * product.quantity);
-// }
+// const logProduct = product => console.log(product);
+// const logTotalPrice = product => console.log(product.price * product.quantity);
 
 // createProduct({ name: '🍎', price: 30, quantity: 3 }, logProduct);
 // createProduct({ name: '🍋', price: 20, quantity: 5 }, logTotalPrice);
@@ -65,36 +63,54 @@ const createProduct = product => {};
 /*
 ? Виконайте рефакторинг коду за допомогою стрілочних функцій.
 */
-// function each(array, callback) {
+// const each = (array, callback) => {
+//   // let callback = function (value) {
+//   //   return value * 2;
+//   // }
+
 //   const newArr = [];
+
 //   for (const el of array) {
 //     newArr.push(callback(el));
 //   }
-//   return newArr;
-// }
 
+//   return newArr;
+// };
+
+// Повна версія
 // console.log(
-//   each([64, 49, 36, 25, 16], function (value) {
+//   each([64, 49, 36, 25, 16], value => {
 //     return value * 2;
 //   })
 // );
+
 // console.log(
-//   each([64, 49, 36, 25, 16], function (value) {
+//   each([64, 49, 36, 25, 16], value => {
 //     return value - 10;
 //   })
 // );
+
 // console.log(
-//   each([64, 49, 36, 25, 16], function (value) {
+//   each([64, 49, 36, 25, 16], value => {
 //     return Math.sqrt(value);
 //   })
 // );
+
 // console.log(
-//   each([1.5, 2.1, 16.4, 9.7, 11.3], function (value) {
+//   each([1.5, 2.1, 16.4, 9.7, 11.3], value => {
 //     return Math.ceil(value);
 //   })
 // );
+
 // console.log(
-//   each([1.5, 2.1, 16.4, 9.7, 11.3], function (value) {
+//   each([1.5, 2.1, 16.4, 9.7, 11.3], value => {
 //     return Math.floor(value);
 //   })
 // );
+
+// Скорочена версія
+// console.log(each([64, 49, 36, 25, 16], value => value * 2));
+// console.log(each([64, 49, 36, 25, 16], value => value - 10));
+// console.log(each([64, 49, 36, 25, 16], value => Math.sqrt(value)));
+// console.log(each([1.5, 2.1, 16.4, 9.7, 11.3], value => Math.ceil(value)));
+// console.log(each([1.5, 2.1, 16.4, 9.7, 11.3], value => Math.floor(value)));
--- js/for-each-method.js
+++ js/for-each-method.js
@@ -5,25 +5,46 @@
 /*
 ? Виконайте рефакторинг коду, використовуючи метод forEach і стрілочні функції.
 */
-// function logItems(items) {
+// const logItems = items => {
 //   console.log(items);
-//   for (let i = 0; i < items.length; i += 1) {
-//     console.log(`${i + 1} - ${items[i]}`);
-//   }
-// }
+
+//   // for (let i = 0; i < items.length; i += 1) {
+//   //   console.log(`${i + 1} - ${items[i]}`);
+//   // }
+
+//   // Повна версія
+//   // items.forEach((el, idx) => {
+//   //   console.log(`${idx + 1} - ${el}`);
+//   // });
+
+//   // Скорочена версія
+//   items.forEach((el, idx) => console.log(`${idx + 1} - ${el}`));
+// };
 
 // logItems(['Mango', 'Poly', 'Ajax']);
 // logItems(['🍎', '🍇', '🍑', '🍌', '🍋']);
 /*
 ? Виконайте рефакторинг коду, використовуючи метод forEach і стрілочні функції.
 */
-// function printContactsInfo({ names, phones }) {
+// const printContactsInfo = ({ names, phones }) => {
 //   const nameList = names.split(',');
 //   const phoneList = phones.split(',');
-//   for (let i = 0; i < nameList.length; i += 1) {
-//     console.log(`${nameList[i]}: ${phoneList[i]}`);
-//   }
-// }
+
+//   console.log(nameList);
+//   console.log(phoneList);
+
+//   // for (let i = 0; i < nameList.length; i += 1) {
+//   //   console.log(`${nameList[i]}: ${phoneList[i]}`);
+//   // }
+
+//   // Повна версія
+//   // nameList.forEach((el, idx, arr) => {
+//   //   console.log(`${el}: ${phoneList[idx]}`);
+//   // });
+
+//   // Скорочена версія
+//   nameList.forEach((el, idx) => console.log(`${el}: ${phoneList[idx]}`));
+// };
 
 // printContactsInfo({
 //   names: 'Jacob,William,Solomon,Artemis',
@@ -33,14 +54,24 @@
 /*
 ? Виконайте рефакторинг коду, використовуючи метод forEach і стрілочні функції.
 */
-// function calсulateAverage(...args) {
+// const calculateAverage = (...args) => {
 //   let total = 0;
-//   for (let i = 0; i < args.length; i++) {
-//     total += args[i];
-//   }
-//   return total / args.length;
-// }
 
-// console.log(calсulateAverage(1, 2, 3, 4)); // 2.5
-// console.log(calсulateAverage(14, 8, 2)); // 8
-// console.log(calсulateAverage(27, 43, 2, 8, 36)); // 23.2
+//   // for (let i = 0; i < args.length; i += 1) {
+//   //   total += args[i];
+//   // }
+
+//   // Повна версія
+//   // args.forEach((el, idx, arr) => {
+//   //   total += el;
+//   // });
+
+//   // Скорочена версія
+//   args.forEach(el => (total += el));
+
+//   return total / args.length;
+// };
+
+// console.log(calculateAverage(1, 2, 3, 4)); // 2.5
+// console.log(calculateAverage(14, 8, 2)); // 8
+// console.log(calculateAverage(27, 43, 2, 8, 36)); // 23.2
