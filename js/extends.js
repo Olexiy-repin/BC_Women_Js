f2cc0493c1e46360c51009bc9a9a52c3efe9e89e
Olexiy-repin <repinlyoha@gmail.com>
2022-12-23 12:19:49 +0200

    classwork

---
+++ js/extends.js
@@ -0,0 +1,94 @@
+/*
+ * Наслідування з extends та super
+ */
+
+// class User {
+//   #login;
+//   #email;
+
+//   constructor({ firstName, lastName, age, login, email }) {
+//     // let this = {};
+//     this.firstName = firstName;
+//     this.lastName = lastName;
+//     this.userAge = age;
+//     this.#login = login;
+//     this.#email = email;
+//     // return this;
+//   }
+
+//   get login() {
+//     return this.#login;
+//   }
+
+//   set login(newLogin) {
+//     this.#login = newLogin;
+//   }
+
+//   get email() {
+//     return this.#email;
+//   }
+
+//   set email(newEmail) {
+//     this.#email = newEmail;
+//   }
+
+//   getFullName() {
+//     return `${this.firstName} ${this.lastName}`;
+//   }
+// }
+
+// class Developer extends User {
+//   constructor(developerInfo) {
+//     const { countOfProjects, ...otherProps } = developerInfo;
+
+//     super(otherProps);
+
+//     this.countOfProjects = countOfProjects;
+//   }
+
+//   doDeveloperWork() {
+//     console.log(`Роблю якусь роботу`);
+//   }
+// }
+
+// class Manager extends User {
+//   constructor(managerInfo) {
+//     const { managerProp, ...otherProps } = managerInfo;
+
+//     super(otherProps);
+
+//     this.managerProp = managerProp;
+//   }
+
+//   doManagerWork() {
+//     console.log(`Роблю якусь роботу`);
+//   }
+// }
+
+// const user = new Developer({
+//   countOfProjects: 10,
+//   firstName: 'Nicholas',
+//   lastName: 'West',
+//   age: 30,
+//   login: 'vcvd',
+//   email: 'iseegi@sak.sx',
+// });
+
+// console.log(user);
+
+// console.log(user.getFullName());
+
+// const someManager = new Manager({
+//   managerProp: 343,
+//   firstName: 'Caleb',
+//   lastName: 'Murray',
+//   age: 20,
+//   login: 'bgff',
+//   email: 'uwigicnaf@ra.be',
+// });
+
+// console.log(someManager);
+
+// console.log(someManager.getFullName());
+
+console.log(document.querySelector('style'));
