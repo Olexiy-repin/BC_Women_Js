74de9ab3c94feeb9411ce5457fc88e3ffa6b52f0
Olexiy-repin <repinlyoha@gmail.com>
2023-01-12 10:02:03 +0200

    preparation for class work

---
+++ src/js/templates/gallery-cards.js
@@ -0,0 +1,7 @@
+/*
+<li class="gallery__item">
+    <img src="{{this.urls.regular}}" alt="{{this.alt_description}}" class="gallery-img">
+</li>
+*/
+
+export const createGalleryCards = cardInfo => {};
---
+++ src/js/templates/posts-cards.js
@@ -0,0 +1,9 @@
+/* 
+<li class="posts__item">
+    <h2 class="posts__title">{{this.title}}</h2>
+    <p class="posts__text">{{this.body}}</p>
+    <p class="posts__id">id: {{this.id}}</p>
+</li>
+*/
+
+export const createPostsCards = cardInfo => {};
