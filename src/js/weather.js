13b841d92f455c0eb6315a09870c47369096e959
Olexiy-repin <repinlyoha@gmail.com>
2023-01-12 11:29:46 +0200

    classwork

--- src/js/weather.js
+++ src/js/weather.js
@@ -1,10 +1,41 @@
 // https://openweathermap.org/current
 import { fetchWeather } from './weatherApi.js';
+import { createWeatherCard } from './weather-card.js';
 
-// const convertSecondsToHoursAndMinutes = seconds => {
-//   const date = new Date(seconds * 1000);
+const searchFormEl = document.querySelector('.js-search-form');
+const weatherCardEl = document.querySelector('.js-weather__wrapper');
 
-//   return `${new String(date.getHours()).padStart(2, 0)}:${new String(
-//     date.getMinutes()
-//   ).padStart(2, 0)}`;
-// };
+const convertSecondsToHoursAndMinutes = seconds => {
+  const date = new Date(seconds * 1000);
+
+  return `${new String(date.getHours()).padStart(2, 0)}:${new String(
+    date.getMinutes()
+  ).padStart(2, 0)}`;
+};
+
+const onSearchFormSubmit = event => {
+  event.preventDefault();
+
+  const searchedQuery = event.target.elements.user_country.value.trim();
+
+  fetchWeather(searchedQuery)
+    .then(data => {
+      data.sys.sunrise = convertSecondsToHoursAndMinutes(data.sys.sunrise);
+      data.sys.sunset = convertSecondsToHoursAndMinutes(data.sys.sunset);
+
+      weatherCardEl.innerHTML = createWeatherCard(data);
+    })
+    .catch(err => {
+      switch (err.message) {
+        case '404': {
+          alert('За вашим запитом нічого не знайдено!');
+          break;
+        }
+      }
+    })
+    .finally(() => {
+      event.target.reset();
+    });
+};
+
+searchFormEl.addEventListener('submit', onSearchFormSubmit);
--- src/js/weatherApi.js
+++ src/js/weatherApi.js
@@ -1,11 +1,21 @@
-export const fetchWeather = cityName => {
-  return fetch(
-    `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&units=metric&lang=ua&appid=95632b02f9162f375a368971925f5209`
-  ).then(response => {
-    if (!response.ok) {
-      throw new Error(response.status);
-    }
+const BASE_URL = 'https://api.openweathermap.org';
+const API_KEY = '95632b02f9162f375a368971925f5209';
 
-    return response.json();
+export const fetchWeather = cityName => {
+  const searchParams = new URLSearchParams({
+    q: cityName,
+    units: 'metric',
+    lang: 'ua',
+    appid: API_KEY,
   });
+
+  return fetch(`${BASE_URL}/data/2.5/weather?${searchParams}`).then(
+    response => {
+      if (!response.ok) {
+        throw new Error(response.status);
+      }
+
+      return response.json();
+    }
+  );
 };
