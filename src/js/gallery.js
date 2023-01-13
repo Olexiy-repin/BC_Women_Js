eeb9ede883e6c6cbed301b37ff6e560a02d7e541
Olexiy-repin <repinlyoha@gmail.com>
2023-01-13 12:49:39 +0200

    classwork

---
+++ src/js/gallery.js
@@ -0,0 +1,71 @@
+// https://unsplash.com/
+import { UnsplashAPI } from './unsplash-api.js';
+import { createGalleryCards } from './templates/gallery-cards.js';
+
+const searchFormEl = document.querySelector('.js-search-form');
+const galleryListEl = document.querySelector('.js-gallery');
+const loadMoreBtnEl = document.querySelector('.js-load-more');
+const searchBtnEl = document.querySelector('.js-search-btn');
+
+const unsplashAPI = new UnsplashAPI();
+
+const onSearchFormSubmit = event => {
+  event.preventDefault();
+
+  searchBtnEl.disabled = true;
+
+  unsplashAPI.query = event.target.elements.user_search_query.value;
+  unsplashAPI.page = 1;
+
+  unsplashAPI
+    .fetchPhotosByQuery()
+    .then(data => {
+      if (data.results.length === 0) {
+        alert('За вашим запитом нічого не знайдено');
+        event.target.reset();
+        galleryListEl.innerHTML = '';
+
+        return;
+      }
+
+      if (data.total_pages > 1) {
+        loadMoreBtnEl.classList.remove('is-hidden');
+      }
+
+      galleryListEl.innerHTML = createGalleryCards(data.results);
+    })
+    .catch(err => {
+      console.log(err);
+    })
+    .finally(() => {
+      searchBtnEl.disabled = false;
+    });
+};
+
+const onLoadMoreBtnClick = event => {
+  event.target.disabled = true;
+
+  unsplashAPI.page += 1;
+
+  unsplashAPI
+    .fetchPhotosByQuery()
+    .then(data => {
+      galleryListEl.insertAdjacentHTML(
+        'beforeend',
+        createGalleryCards(data.results)
+      );
+
+      if (data.total_pages === unsplashAPI.page) {
+        loadMoreBtnEl.classList.add('is-hidden');
+      }
+    })
+    .catch(err => {
+      console.log(err);
+    })
+    .finally(() => {
+      event.target.disabled = false;
+    });
+};
+
+searchFormEl.addEventListener('submit', onSearchFormSubmit);
+loadMoreBtnEl.addEventListener('click', onLoadMoreBtnClick);
---
+++ src/js/micro-macro-tasks.js
@@ -0,0 +1,37 @@
+// http://latentflip.com/loupe/?code=Y29uc29sZS5sb2coJ2ZpcnN0Jyk7CgpzZXRUaW1lb3V0KGZ1bmN0aW9uKCkgewogICAgY29uc29sZS5sb2coJ2lubmVyIHNldFRpbWVvdXQnKTsKfSwgMTAwMCk7Cgpjb25zb2xlLmxvZygnc2Vjb25kJyk7!!!
+
+// console.log(1);
+
+// setTimeout(() => console.log(2), 0);
+
+// Promise.resolve().then(() => console.log(3));
+
+// Promise.resolve().then(() => setTimeout(() => console.log(4), 0));
+
+// Promise.resolve().then(() => console.log(5));
+
+// setTimeout(() => console.log(6), 0);
+
+// console.log(7);
+
+// const counterOutputEl = document.querySelector('.js-counter');
+
+// for (let i = 1; i <= 1000; i += 1) {
+//   counterOutputEl.innerHTML = i;
+// }
+
+// let i = 0;
+
+// const count = () => {
+//   i += 5;
+
+//   counterOutputEl.innerHTML = i;
+
+//   if (i !== 1000) {
+//     setTimeout(() => {
+//       count();
+//     }, 100);
+//   }
+// };
+
+// count();
